e5ca68abf58bae0fb63f2cb38511263b4beae93e
Bhavishya <bhavishyachowdary185@gmail.com>
2024-08-28 15:04:41 -0400

    shell

--- frontend.sh
+++ frontend.sh
@@ -56,4 +56,7 @@ VALIDATE $? "Extract frontend code"
 
 cp /home/ec2-user/expense-shell/expense.config/etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense conf"
-systemctl restart nginx
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarted Nginx"
+
